98087a4e57ccdb3f5238116fa81d70af290ac476
Ritesh Prask <riteshprask@gmail.com>
2026-05-14 15:31:02 +0530

    Add files via upload

---
+++ 04052026_P2.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import Runnable, RunnableLambda
+
+# Ensure API Key is set
+# Using dotenv to fetch Google API Key from .env file
+load_dotenv()
+
+# Initialize the model
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+# First chain generates a story
+story_prompt = ChatPromptTemplate.from_template("Write a short story about {topic}")
+story_chain = story_prompt | llm | StrOutputParser()
+
+# Second chain analyzes the story
+analysis_prompt = ChatPromptTemplate.from_template('Analyse the mood of the following: \n{story}')
+analysis_chain = analysis_prompt | llm | StrOutputParser()
+
+# Combine chains
+story_with_analysis = story_chain | analysis_chain
+
+# Run the combined chain
+story_analysis = story_with_analysis.invoke({"topic" : "a rainy day"})
+print("\n Analysis:", story_analysis)
