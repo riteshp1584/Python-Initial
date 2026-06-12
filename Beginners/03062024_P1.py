3266704e6c386c02c54e25c37587a9c57e42fb89
Ritesh Prask <riteshprask@gmail.com>
2026-06-12 21:34:00 +0530

    Create 03062024_P1.py

---
+++ Beginners/03062024_P1.py
@@ -0,0 +1,25 @@
+# Exercises in NumPy
+
+import numpy as np
+
+x = np.arange(1,6)
+
+y = np.arange(11,16)
+
+add1 = x + y
+
+print(add1)
+
+sub1 = y - x
+
+print(sub1)
+
+mul1 = x * y
+
+print(mul1)
+
+div1 = x / y
+
+print(div1)
+
+# Works correctly until here
