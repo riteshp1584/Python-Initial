a7fb22eccc1ed50b83e56be584c82bc395b5a650
Ritesh Prask <riteshprask@gmail.com>
2026-06-14 21:19:48 +0530

    Create 03062024_P3.py

---
+++ Beginners/03062024_P3.py
@@ -0,0 +1,13 @@
+# Exercises in NumPy
+
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+
+arr2 = np.array([4, 5, 6])
+
+arr3 = np.concatenate((arr1, arr2))
+
+print(arr3)
+
+# Works correctly until here
