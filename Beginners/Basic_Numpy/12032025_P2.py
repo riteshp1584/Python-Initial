c071b0fe8518c7c4a45bfb09c3c4564c9166cb01
Ritesh Prask <riteshprask@gmail.com>
2026-06-24 16:31:51 +0530

    Array indexing and slicing examples
    Added a Python script demonstrating basic NumPy operations including array creation and indexing.

---
+++ Beginners/Basic_Numpy/12032025_P2.py
@@ -0,0 +1,20 @@
+# Exercises in NumPy
+
+import numpy as np
+
+matrix = np.array([[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, 9],
+                   [10, 11, 12]])
+
+vector = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+print(vector[2])
+
+print(vector[:6])
+
+print(vector[2:6])
+
+print(matrix[1,1])
+
+print(matrix[:3,:])
