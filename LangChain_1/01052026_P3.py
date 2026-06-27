e36bb55df0e91030e45853f124440314746450d0
Ritesh Prask <riteshprask@gmail.com>
2026-06-27 20:59:57 +0530

    Removed LangChain_1 after moving to Python-Agentic

--- LangChain_1/01052026_P3.py
+++
@@ -1,8 +0,0 @@
-
-from langchain_community.llms import FakeListLLM
-
-fake_llm = FakeListLLM(responses=["Always this response!"])
-
-result = fake_llm.invoke("What is the best way to tour Europe?")
-
-print(result)
